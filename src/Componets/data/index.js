7c2c51a1fd49a424011b5822d54e1e7472e63653
Vikalp Nag <vikalpnag@gmail.com>
2025-07-01 03:10:35 +0530

    Data for suggestions

---
+++ src/Componets/data/index.js
@@ -0,0 +1,13 @@
+export const suggestions = [{
+    id: "01",
+    que: "Who wrote brown rang?"
+}, {
+    id: "02",
+    que: "Billo bagge billeya da ki karegi?"
+}, {
+    id: "03",
+    que: "Where I can get kasoori methi?"
+}, {
+    id: "04",
+    que: "Why to use 100% of your brain?"
+}]
