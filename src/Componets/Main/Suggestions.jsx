aa28198897e465c5b9651ed16c404215daf1f519
Vikalp Nag <vikalpnag@gmail.com>
2025-07-05 03:33:45 +0530

    Moreeee updates

--- src/Componets/Main/Suggestions.jsx
+++ src/Componets/Main/Suggestions.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { suggestions } from "../data";
 import "./suggestion.css";
+import { useContext } from "react";
+import { Context } from "../../context/context";
 
 const Suggestions = () => {
+  const { setInput, onSent } = useContext(Context);
+
   return (
     <div className="suggestions-grid">
       {suggestions.map((item) => (
-        <div className="card" key={item.id} onClick={() => {}}>
+        <div
+          className="card"
+          key={item.id}
+          onClick={() => {
+            onSent(item.que);
+          }}
+        >
           <img src={item.img} alt="icon" />
           <p>{item.que}</p>
         </div>
